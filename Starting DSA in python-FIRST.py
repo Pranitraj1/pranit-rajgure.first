5781ba99d5050e5e28b81868726fbb19e105a664
Pranitraj1 <141752416+Pranitraj1@users.noreply.github.com>
2023-09-09 23:10:53 +0530

    Add files via upload

---
+++ Starting DSA in python-FIRST.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+print("Hello World")
+print("Pranit Rajgure")
+
+
+# In[7]:
+
+
+# Variables
+a = 20
+b = 40
+print((a+b))
+
+c = "Pranit Rajgure"
+print(c)
+
+
+# In[11]:
+
+
+# Data Types
+a = 20
+b = "coding"
+c = 2.3
+print(type(c))
+print(type(a))
+print(type(b))
+
+
+# In[14]:
+
+
+# arithmetic operator
+a = 17
+b = 10
+print(a/b)
+print(a//b)    
+# it gives floor values, the floor values of 1.7 is 1
+
+
+
+# In[16]:
+
+
+# input
+a = int(input())
+print(a)
+print(type(a))
+
+
+# In[ ]:
+
+
+
+
